759e7dc5066a3f03dd5494c62acf4f41cf2d9aca
WillHuss2008 <williamhss2008@gmail.com>
2024-11-07 09:35:40 -0600

    done

--- speedtest.sh
+++ speedtest.sh
@@ -18,20 +18,11 @@ upload=$(echo "$speed" | grep "Upload" | awk {'print int($2)'} | grep -v Mbit)
 if [[ $upload -le 40 ]]; then
     up_rev="bad"
 else
-<<<<<<< HEAD
-    up_rev="good"    
-=======
     up_rev="good"
->>>>>>> 32e22444ddb23f717e3751f4ef22af4d8a2001e9
 fi
 
 #leaving a review
 if [[ $download -le 40 || $upload -le 40 ]]; then
-<<<<<<< HEAD
-    rev="Your download speed is $down_rev but your upload speed is $up_rev"
-else
-    rev="Your download speed is $down_rev and your upload speed is $up_rev"
-=======
     echo "$(date): $USER
 $speed
     
@@ -46,14 +37,4 @@ $speed
 Your downlad speed is $down_rev and your upload speed is $up_rev
     
     " >> $log
->>>>>>> 32e22444ddb23f717e3751f4ef22af4d8a2001e9
 fi
-
-log=speedlog.txt
-echo "
-$(date)
-Network: $(sudo nmcli dev wifi show | awk {'print $2'} | sed -n '1p')
-$speed
-$rev
-performed by $USER
-" >> $log

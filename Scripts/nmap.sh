bc251edfe7223b1598f50c696242000dc92e3e80
WillHuss2008 <willhuss2008@gmail.com>
2025-02-21 09:57:47 -0600

    done

--- Scripts/nmap.sh
+++ Scripts/nmap.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
-
-network=$(ip addr | grep "inet " | awk {'print $2'} | sed -n 2p)
-echo "$network"
-nmap -sn $network | awk '
-/^Nmap scan report/ { ip=$NF }
-/^Mac Address:/ { mac=$3; name=substr($0, index($0, $4)); printf "%s - %s - %s\n", ip, mac, name }'
+if [[ $(id -u) -ne 0 ]]; then
+    echo "permission denied"
+    exit 0
+fi
+echo "please enter the gateway ip to scan"
+read network
+echo "please enter the network's bit number"
+read bit
+echo "scan 1
+" > /home/pi/Desktop/Class/Text_Files/scannedfor1
+scan1=$(sudo nmap -sn $network/$bit | awk '
+/^Nmap scan report/ {ip=$NF} /^MAC Address/ {mac=$3; name=substr($0, index($0, $4)); printf "%s - %s - %s\n", ip, mac, name}')
+echo "$scan1" >> /home/pi/Desktop/Class/Text_Files/scannedfor1
+echo "please press the enter key when ready to continue"
+read n
+echo "scan 2
+" > /home/pi/Desktop/Class/Text_Files/scannedfor2
+scan2=$(sudo nmap -sn $network/$bit | awk '
+/^Nmap scan report/ {ip=$NF} /^MAC Address/ {mac=$3; name=substr($0, index($0, $4)); printf "%s - %s - %s\n", ip, mac, name}')
+echo "$scan2" >> /home/pi/Desktop/Class/Text_Files/scannedfor2
+diff -y /home/pi/Desktop/Class/Text_Files/scannedfor1 /home/pi/Desktop/Class/Text_Files/scannedfor2
+echo "would you like to remove the scan files?"
+read answer
+if [[ "$answer" = "yes" ]]; then
+    sudo rm /home/pi/Desktop/Class/Text_Files/scannedfor1 /home/pi/Desktop/Class/Text_Files/scannedfor2
+fi
